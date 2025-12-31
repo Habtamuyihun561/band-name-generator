2ae0a407672002a860762cd57fe07f35a3c1cd6e
Habtamu Yihun <habtiefather@gmail.com>
2025-12-31 09:31:45 +0100

    initial commit: band name generator

---
+++ band_name_generator.py
@@ -0,0 +1,11 @@
+"""
+This project focuses on automatically generating a band name
+; it asks a city that  you grew up in and name of your pet,
+ and it combines those together  and  gives you the name of your band
+"""
+print("Welcome to the Band Name Generator Mini Project")
+name_of_city = input("What is the name of the city you grew up in?\n")
+print(name_of_city)
+pet_name = input("What is the name of the pet's you grew up in?\n")
+print(pet_name)
+print("you band name could be" + name_of_city + "" + pet_name)
